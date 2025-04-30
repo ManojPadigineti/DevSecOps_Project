d71c8125aa071671a74026a192fc998732d11c86
Manoj <padigineti.manojkumar@gmail.com>
2025-04-30 22:45:22 +0530

    modified

--- ansible_terraform/ansible_run.sh
+++ ansible_terraform/ansible_run.sh
@@ -1,7 +1,7 @@
 run () {
     for instances in $@ 
       do
-         ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=$run -e role_name=$run
+         ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=$instances
       done
 }
 
